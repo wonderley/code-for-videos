324ab179fa93f91396cd4d28e6088d0626a08f58
Lucas Wonderley <lucas.wonderley@samsara.com>
2021-03-22 23:52:29 -0400

    changes

--- go-fish/frontend/src/App.js
+++ go-fish/frontend/src/App.js
@@ -2,17 +2,17 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <div style={{
-          width: "64px",
-          height: "89px",
-          backgroundColor: "white",
-          borderRadius: "5px",
-          display: "flex",
+          width: '64px',
+          height: '89px',
+          backgroundColor: 'white',
+          borderRadius: '5px',
+          display: 'flex',
         }}>
           <span style={{
-            margin: "auto",
+            margin: 'auto',
           }}>10♠️</span>
         </div>
       </header>
