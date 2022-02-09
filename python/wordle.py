ca9f6d39573b3cce98a0dea9d261ccbc266e63f1
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2022-02-09 07:37:08 -0500

    validate guesses in a loop

--- python/wordle.py
+++ python/wordle.py
@@ -8,6 +8,17 @@ def get_five_letter_words(words):
             five_letter_words.add(word)
     return five_letter_words
 
+def prompt_for_guess(valid_words):
+    while True:
+        guess = input()
+        if len(guess) != 5:
+            print('Please guess a five-letter word.')
+        elif guess not in valid_words:
+            print(f'{guess} is not a valid word.')
+        else:
+            return guess
+
+
 words = get_five_letter_words(english_words_lower_alpha_set)
 
 secret_word = random.choice(list(words))
@@ -17,8 +28,8 @@ print('Guess a five letter word.')
 print(f'The word is {secret_word}')
 n_guesses = 6
 guessed_word = False
-for i in range(n_guesses):
-    guess = input()
+for i in range(n_guesses): 
+    guess = prompt_for_guess(words)
     if guess == secret_word:
         guessed_word = True
         break
