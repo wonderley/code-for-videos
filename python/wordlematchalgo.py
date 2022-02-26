102a0c7b842238461cbf0bae2fef54012aceb7f8
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2022-02-26 14:08:40 -0500

    fix wordle match algo

--- python/wordlematchalgo.py
+++ python/wordlematchalgo.py
@@ -3,13 +3,22 @@ NONE = 0
 GREEN = 1
 YELLOW = 2
 def find_position_matches(target, guess):
-    target_letters = set(target)
+    matched_target_indexes = set()
     position_colors = [NONE] * len(target)
     for i, l in enumerate(guess):
         if target[i] == l:
             position_colors[i] = GREEN
-        elif l in target_letters:
-            position_colors[i] = YELLOW
+            matched_target_indexes.add(i)
+    for gi, gl in enumerate(guess):
+        for ti, tl in enumerate(target):
+            if gi == ti or ti in matched_target_indexes:
+                # Don't color letters yellow if 
+                # the target is already green
+                # in its own position
+                continue
+            elif gl == tl:
+                position_colors[gi] = YELLOW
+                break
     return position_colors
 
 if __name__ == '__main__':
