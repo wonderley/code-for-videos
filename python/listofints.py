d9b0b080ad4bed97e1bdebbeb6066ed532542069
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2022-03-02 08:43:16 -0500

    list comprehension

--- python/listofints.py
+++ python/listofints.py
@@ -1,8 +1,6 @@
 ints_from_input = input('enter some ints! ')
 list_of_strings = ints_from_input.split(' ')
-list_of_ints = []
-for s in list_of_strings:
-    list_of_ints.append(int(s))
+list_of_ints = [int(s) for s in list_of_strings]
 print(list_of_ints)
 
 
