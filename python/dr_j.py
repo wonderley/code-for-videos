a01ce03e861dba06726b32b02564a812306c486c
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2022-03-05 08:29:30 -0500

    dictionary with missing item

---
+++ python/dr_j.py
@@ -0,0 +1,8 @@
+player_numbers = {
+    'Michael Jordan': 23,
+    'LeBron James': 6,
+    'Kobe Bryant': 24,
+}
+dr_j_number = player_numbers.get('Julius Erving', 'not found')
+print(f"Dr J.'s number was {dr_j_number}.")
+
