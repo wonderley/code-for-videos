f68e8b52ba130da0d5383ca92f56d1052e214e67
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2022-03-09 22:16:49 -0500

    remove old wordle file

--- python/wordleflow.py
+++
@@ -1,8 +0,0 @@
-print('~~WORDLE~~')
-print('Guess a five letter word.')
-n_guesses = 6
-for i in range(n_guesses):
-    guess = input()
-    print(f"The word's not {guess}.")
-    print(f'You have {n_guesses-1-i} guesses left')
-print("Sorry, you didn't guess the word!")
