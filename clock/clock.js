600e9e981780c851684e598d286b583b69d4f143
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2022-03-30 08:49:18 -0400

    recent updates to clock app

--- clock/clock.js
+++ clock/clock.js
@@ -1,9 +1,9 @@
 function updateClock() {
     const current = new Date()
     const clock = document.getElementById('clock')
-    const h = current.getHours()
-    const m = current.getMinutes()
-    const s = current.getSeconds()
+    const h = String(current.getHours()).padStart(2, '0')
+    const m = String(current.getMinutes()).padStart(2, '0')
+    const s = String(current.getSeconds()).padStart(2, '0')
     clock.innerHTML = `${h}:${m}:${s}`
 }
 
